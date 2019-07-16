506241cc211822e332c365dc8555413297eef5aa
annettesunday <annette.odhiambo@andela.com>
2019-07-16 16:35:57 +0300

    ch(ResourceList): fetching a given resource

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,8 +1,26 @@
 import React from "react";
+import axios from "axios";
 
 class ResourceList extends React.Component {
+  state = {
+    resources: []
+  };
+  async componentDidMount() {
+    const response = await axios.get(
+      `http://jsonplaceholder.typicode.com/${this.props.resource}`
+    );
+
+    this.setState({ resources: response.data });
+  }
+
+  async componentDidUpdate(prevProps) {
+    const response = await axios.get(
+      `http://jsonplaceholder.typicode.com/${this.props.resource}`
+    );
+    this.setState({ resources: response.data });
+  }
   render() {
-    return <div>{this.props.resource}</div>;
+    return <div>{this.state.resources.length}</div>;
   }
 }
 

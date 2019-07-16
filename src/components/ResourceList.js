2cad2a332c6dadeb3d6d41088cfb2ac3b4d53b62
annettesunday <annette.odhiambo@andela.com>
2019-07-16 17:07:26 +0300

    ch(ResourceList): updates with useEffect

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -11,9 +11,10 @@ const ResourceList = ({ resource }) => {
     setResources(response.data);
   };
 
+//   arrow function is only called when the values are different
   useEffect(() => {
     fetchResource(resource);
-  }, []);
+  }, [resource]);
 
   return <div>{resources.length}</div>;
 };

77d866d627454483c97cf6be652ba119ae14b838
annettesunday <annette.odhiambo@andela.com>
2019-07-16 16:04:18 +0300

    ch(App): building stateful class components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,28 @@
 import React from "react";
 
 class App extends React.Component {
+  state = {
+    resource: "posts"
+  };
   render() {
-    return <div>App</div>;
+    return (
+      <div className="ui container">
+        <button
+          onClick={() => this.setState({ resource: "posts" })}
+          className="ui button primary"
+        >
+          Posts
+        </button>
+        <button
+          onClick={() => this.setState({ resource: "todos" })}
+          className="ui button primary"
+        >
+          Todos
+        </button>
+        <br />
+        {this.state.resource}
+      </div>
+    );
   }
 }
 
